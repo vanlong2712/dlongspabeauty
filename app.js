a9c8e259a3f6d441076909937cfaf3d132ccf80d
DESKTOP-RM6ETRP\vanlong2712 <d.long271293@gmail.com>
2017-09-15 19:34:41 +0700

    commit continue

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var flash= require('connect-flash');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
+var toastr = require('express-toastr');
+const Passport=require('Passport');
+const LocalStrategy=require('Passport-local').Strategy;
 var bodyParser = require('body-parser');
 /*connect to mongoose*/
-global.mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/spabeauty');
+const {mongoose} = require('./db/mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var admin = require('./routes/admin');
+var login_admin = require('./routes/login_admin');
+var admin_service = require('./routes/admin_service');
 
 var app = express();
 
@@ -30,11 +35,20 @@ app.use(session({
     saveUninitialized: true,
     resave: true
 }));
+app.use(Passport.initialize());
+app.use(Passport.session());
+app.use(flash());
+// Load express-toastr
+// You can pass an object of default options to toastr(), see example/index.coffee
+app.use(toastr());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
 app.use('/187_admin', admin);
+app.use('/187_admin', login_admin);
+app.use('/187_admin', admin_service);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- vendor/admin_script.js
+++ vendor/admin_script.js
@@ -23,6 +23,41 @@ var delete_img = async (image) => {
   });
 }
 
+var errorToaStr = async (req,msg) => {
+  console.log('errorToaStr');
+  await req.toastr.error(msg);
+}
+
+var successToaStr = async (req,msg) => {
+  console.log('successToaStr');
+  await req.toastr.success(msg);
+}
+
+var successToaStr = async (req,msg,title) => {
+  console.log('successToaStr');
+  await req.toastr.success(msg,title);
+}
+
+var authenticateAdmin = (req, res) => {
+  if(!req.isAuthenticated() || req.user.role !== 1) {
+    res.redirect('/187_admin/login');
+  }
+}
+
+var priviledgeService = (req, res) => {
+  if(!req.user.isPriviledge.includes('service')) {
+    req.toastr.warning('Bạn chưa có quyền truy cập vào Dịch Vụ');
+    res.redirect('/187_admin/');
+  }
+}
+
+var priviledgeAdmin = (req, res) => {
+  if(!req.user.isPriviledge.includes('admin')) {
+    req.toastr.warning('Bạn chưa có quyền truy cập vào Quản Lý Admin');
+    res.redirect('/187_admin/');
+  }
+}
+
 // var image_service_handle = (image, service_img,array=[]) => {
 //   return new Promise((resolve, reject) => {
 //     if(image !== '') {
@@ -45,5 +80,10 @@ var delete_img = async (image) => {
 
 module.exports = {
   image_service_handle,
-  delete_img
+  delete_img,
+  successToaStr,
+  errorToaStr,
+  authenticateAdmin,
+  priviledgeService,
+  priviledgeAdmin
 }
