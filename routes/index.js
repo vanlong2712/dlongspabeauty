6c95552c36e22047ed93fb9810e4e993d5ddf12f
DESKTOP-RM6ETRP\vanlong2712 <d.long271293@gmail.com>
2017-09-17 12:33:23 +0700

    heroku push

--- routes/index.js
+++ routes/index.js
@@ -5,27 +5,22 @@ var Service = require('../model/service');
 
 
 /* GET home page. */
-// router.get('/', async (req, res, next) => {
-//   var services = await Service.find();
-//   res.render('index', { title: 'Beauty Spa', services: services});
-// });
-
-router.get('/', function (req, res, next) {
-  Service.find().then((services) => {
-    res.render('index', { title: 'Beauty Spa', services: services});
-  })
+router.get('/', async (req, res, next) => {
+  var services = await Service.find();
+  res.render('index', { title: 'Beauty Spa', services: services});
 })
+
 
 /* GET services page. */
 router.get('/services', async (req, res, next) => {
   var services = await Service.find();
   res.render('services', { services: services });
-});
+})
 
 /* GET service_detail page. */
 router.get('/service/:shortid', async (req, res, next) => {
   var service = await Service.findOne({shortid: req.params.shortid});
   res.render('service_details', {service: service});
-});
+})
 
 module.exports = router;
