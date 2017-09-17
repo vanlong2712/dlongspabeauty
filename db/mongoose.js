f2c6cb46dc1dfe555a910a905d1fa9cd313d64f9
DESKTOP-RM6ETRP\vanlong2712 <d.long271293@gmail.com>
2017-09-17 11:57:17 +0700

    first heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 global.mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/spabeauty');
-
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/spabeauty');
+mongoose.connect('mongodb://demospabeauty:demospabeauty123@ds141024.mlab.com:41024/heroku_fq5r90hd');
 module.exports = {mongoose};
