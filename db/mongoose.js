623820c05d785d367d0962146672f41a7f104542
DESKTOP-RM6ETRP\vanlong2712 <d.long271293@gmail.com>
2017-09-17 13:13:36 +0700

    heroku push

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,5 +2,5 @@ global.mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/spabeauty');
-mongoose.connect('mongodb://demospabeauty:spabeauty1234@ds141024.mlab.com:41024/heroku_fq5r90hd');
+mongoose.connect('mongodb://demo:demo@ds141264.mlab.com:41264/spabeauty');
 module.exports = {mongoose};
